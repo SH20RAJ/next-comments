06afd2d53d8ab6b9e2cc003b981cab3a23756e20
sh20raj <shaswatraj3@gmail.com>
2024-06-27 19:32:35 +0530

    Initial commit from Create Next App

---
+++ src/Comments.tsx
@@ -0,0 +1,30 @@
+// src/Comments.tsx
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { useTheme } from 'next-themes';
+
+interface CommentsProps {
+  issueTerm: string;
+  repo?: string;
+}
+
+const Comments: React.FC<CommentsProps> = ({ issueTerm, repo = 'shade-cool/next-comments-vault' }) => {
+  const { theme } = useTheme();
+  const commentsSection = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://utteranc.es/client.js';
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    script.setAttribute('repo', repo);
+    script.setAttribute('issue-term', issueTerm);
+    script.setAttribute('theme', theme === 'dark' ? 'github-dark' : 'github-light');
+    commentsSection.current.appendChild(script);
+  }, [issueTerm, theme, repo]);
+
+  return <div ref={commentsSection} />;
+};
+
+export default Comments;
